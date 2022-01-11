2906cfd3e74a934c0cf6f55d140529da0c3681d2
ChaJiTae1023 <wlxo402@naver.com>
2022-01-11 23:35:04 +0900

    #2.1~#2.2 Basic Data type X About Variable :D

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-alert("hi");
+console.log(5 + 2);
+console.log(5 * 2);
+console.log(5 / 2);
+
+//하단 console.log의 계산 값을 바꾸고 싶으면 여기를 조정해주면 됨
+const a = 5;
+const b = 2;
+
+//위의 console.log의 내용과 같음
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+
+const myName = "JiTae";
+console.log("hello "+ myName);
