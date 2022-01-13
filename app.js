5b94be8813739212c7cafd2f2d445c600f211a33
ChaJiTae1023 <wlxo402@naver.com>
2022-01-13 19:37:14 +0900

    #2.5 Contents About Arrays and function

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-const amIFat = true;
-const areYouFat = false;
+//배열이 없다면...
+//전부 작성해야됨
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
 
-console.log(amIFat);
-console.log(areYouFat);
+//이건 daysOfWeek 그냥 요일 쭉 더한거 
+//위 변수들이 없더라면 아래는 오류가 나버리겠지...
+const daysOfWeek = mon + tue + wed + thu + fri + sat + sun
+console.log(daysOfWeek);
 
-const Head = null;
-let something; //만들었지만 값 전달은 없음 -> undefined
-console.log(Head);
-console.log(something);
+//이게 바로 배열
+const daysOfWeekArray = [mon , tue , wed , thu , fri , sat , sun]
+console.log(daysOfWeekArray);
+
+const nonsence = [1,2,"hello",false,null,true,undefined,"JiTae"];
+console.log(nonsence);
+
+const days =["mon" , "tue" , "wed" , "thu" , "fri" , "sat" , "sun"];
+//             0       1       2       3       4       5       6
+console.log(days[5]); // -> sat 이 출력될거임!!
+
+// Add one more array
+days.push("sun");
+console.log(days[6]);
+console.log(days);
