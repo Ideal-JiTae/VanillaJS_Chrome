3afe254651b6758f0778093a08db3eeb772074b6
ChaJiTae1023 <wlxo402@naver.com>
2022-01-17 21:02:56 +0900

    #2.10 About Object Funtion : Made Calculator Made by Object Function...

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-function plus(a,b){
-    console.log(a+b);
-}
-plus(8,60);
+//객체를 이용한 간단한 계산기 만들기
 
-function divide(a,b){
-    console.log(a/b);
-}
-divide(98,20)
-
-//아래 출력시 안뜨는 것을 볼수 있음 why? -> 인자는 함수 안에서만 존재하기 때문이지!!
-//console.log(a,b);
-
-//player object
-const player = {
-    name: "jitae",
-    sayHello:function(ohterPersonsName){
-        console.log("hello " + ohterPersonsName +" Nice to meet you");
+const calculator={
+    add : function(a,b){
+        console.log(a+b);
+    },
+    min : function(a,b){
+        console.log(a-b);
+    },
+    mul:function(a,b){
+        console.log(a*b);
+    },
+    div : function(a,b){
+        console.log(a/b);
+    },
+    squ : function(a,b){
+        console.log(a**b);
     },
 };
 
-console.log(player.name);
-player.sayHello("Nico");
-player.sayHello("lynn");
+calculator.add(6, 2);
+calculator.min(6, 2);
+calculator.mul(6, 2);
+calculator.div(6, 2);
+calculator.squ(6, 2);
