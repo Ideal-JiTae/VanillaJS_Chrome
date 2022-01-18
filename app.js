8ca9d2b0ceebafdb6d7a051fb387f881d556a15f
ChaJiTae1023 <wlxo402@naver.com>
2022-01-19 04:44:47 +0900

    #2.14 Contents About condition and isNaN Function :D

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-//const age = prompt("How old are you?","Write you age!!")
-//console.log(age);
-
-//아래 차이점을 알고 있어야 함!!
-//console.log(typeof "15",typeof parseInt("15"));
-
-//const age = prompt("How old are you?","Write you age!!")
-//console.log(age,parseInt(age));
-//string / number의 차이
-//만약 age에 숫자가 아닌 값들을 입력하면? parseInt(age)에는 오류가 나게됨
-
 const age = parseInt(prompt("How old are you?"));
-console.log(age);
+
+//만약 true라면 please write a number가 실행
+//만약 false라면 your age = age가 출력됨
+if(isNaN(age)){
+    console.log("Please write a number");
+}else{
+    console.log("your age = "+ age);
+}
