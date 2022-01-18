8c514e2bc895178a0c63ac16cf9b8feb2f610901
ChaJiTae1023 <wlxo402@naver.com>
2022-01-19 05:06:00 +0900

    #2.15 Contents update :D about if-elseif-if and && AND / || OR

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const age = parseInt(prompt("How old are you?"));
 
-//만약 true라면 please write a number가 실행
-//만약 false라면 your age = age가 출력됨
-if(isNaN(age)){
-    console.log("Please write a number");
-}else{
-    console.log("your age = "+ age);
+//문자가 아닌거 입력시 true가 되므로 Please write a number 출력
+//만약 18보다 작은 숫자를 입력했다면 you are yoo young... 출력
+//이 외에 다른 숫자가 입력되었다면 you can drink :D 출력
+//if(isNaN(age)){
+//    console.log("Please write a number");
+//}else if(age <18){
+//    console.log("you are yoo young...");
+//}else{
+//    console.log("you can drink :D");
+//}
+
+//위와 내용이 같으나 아래 것만 해석
+//else if(age >= 18 && age <= 50)
+//18이상이고 50이하이면 you can drink 실행
+if(isNaN(age)||age<0){
+    console.log("Please write a real positive number");
+}else if(age <18){
+    console.log("you are too young...");
+}else if(age >= 18 && age <= 50){
+    console.log("you can drink");
+}else if(age > 50 && age <= 80){
+    console.log("you should exercise...");
+}else {
+    console.log("you are too...old...sorry :<");
 }
+
+true || true === true
+false || true === true
+true || false === true
+false || false === false
+
+true && true === true
+false && true === false
+true && false === false
+false && false === false
