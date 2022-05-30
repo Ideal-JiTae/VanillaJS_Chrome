5896708b73cd7d42b0e30d012b9896abe041a0f1
ChaJiTae1023 <wlxo402@naver.com>
2022-05-30 18:37:03 +0900

    #2.0 JS-HTML

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-const age=parseInt(prompt("write you age"));
-
-if(age===100){
-    console.log("100 years old...?!")
-}else if(age!==100){
-    console.log("you are not ....100 years old... write you age..")
-}
+document.title="Hello From JS!"
