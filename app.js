84a8006ef986c4e001b0e778e37ca8187ed5f94d
ChaJiTae1023 <wlxo402@naver.com>
2022-06-12 23:35:30 +0900

    #3.7 CSS in Javascript 3

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-
-    //변수의 사용
-    const currentColor = h1.style.color;
-    let newColor;
-
-    if(currentColor==="blue"){
-        newColor="tomato";
+    const clickedClass = "clicked";
+    if(h1.className ===clickedClass){
+        h1.className = "";
     }
-    else{
-        newColor ="blue";
-    }
-
-    h1.style.color = newColor;
-
-    //기본 방식
-    // if(h1.style.color==="blue"){
-    //     h1.style.color="tomato";
-    // }
-    // else{
-    //     h1.style.color ="blue";
-    // }
+    else h1.className = clickedClass;
 }
 
 h1.addEventListener("click",handleTitleClick);
