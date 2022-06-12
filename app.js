59aa45121d49f7454d7af12bf10ae1025d479f8b
ChaJiTae1023 <wlxo402@naver.com>
2022-06-13 00:08:54 +0900

    #3.8 CSS in Javascript 4

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
+
+    //아래 두개는 작동결과가 같습니다.
+
+    //classList 함수를 이용하여 구현한것
     const clickedClass = "clicked";
-    if(h1.className ===clickedClass){
-        h1.className = "";
+    if(h1.classList.contains(clickedClass)){ //만약 classList에 contain 포함되어 있다면
+        h1.classList.remove(clickedClass);
     }
-    else h1.className = clickedClass;
+    else h1.classList.add(clickedClass);
+
+    //toggle 로 구현한것
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click",handleTitleClick);
