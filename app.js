abc6e4a4334272ded2fb8789712157c822bdc53d
차지태 <wlxo402@naver.com>
2022-11-05 05:46:49 +0000

    testing

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick(){
-	console.log("hello",loginInput.value);
+	const value = loginInput.value;
+	if(value==""){
+		alert("Please write your name");
+	}
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
+
