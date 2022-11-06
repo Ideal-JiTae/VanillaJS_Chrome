a8074892fc20fa3976a0da9d4bb8ffc51368df9b
차지태 <55056889+ChaJiTae1023@users.noreply.github.com>
2022-11-06 15:11:39 +0900

    setting

--- app.js
+++ app.js
@@ -4,12 +4,6 @@ const loginButton = document.querySelector("#login-form button");
 function onLoginBtnClick(){
 	const username = loginInput.value;
 	console.log(username);
-	//if(username===""){
-	//		alert("Please write your name");
-	//}
-	//else if(username.length > 15){
-	//	alert("Your name is too long");
-	//}
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
