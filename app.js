21041e4bea9e197db289ebd2c2d1e7938d713d85
차지태 <55056889+ChaJiTae1023@users.noreply.github.com>
2022-11-09 20:08:49 +0900

    # 4.7 Super Recap ( 복습 ) + 코드를 줄이는 방법 (매개변수 줄이기 vs localStorage 두번 열어보기)

--- app.js
+++ app.js
@@ -6,14 +6,19 @@ const HIDDEN_CLASSNAME ="hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
+	//사용자가 하는 일을 멈춥니다 -> 값을 제공하면 새로고침이 되는데 페이지새로고침을 막아준다는 뜻
 	event.preventDefault();
+	//Form을 다시 숨겨줍니다.
 	loginForm.classList.add(HIDDEN_CLASSNAME);
-	const username = loginInput.value;
+	//로컬 스토리지에 key와 함께 username에 들어온(loginInput.value)를 저장합니다.
 	localStorage.setItem(USERNAME_KEY,username);
-	paintGreetings(username);
+	//paintGreetings 함수에 username 인자를 넣어 값을 호출합니다.
+	paintGreetings();
 }
 
-function paintGreetings(username){
+//저장하는 순간에 localStorage에 저장된 username의 값을 받아서 username에 저장합니다.
+function paintGreetings(){
+	const username = localStorage.getItem(USERNAME_KEY);
 	greeting.innerText=`Hello ${username}`;
 	greeting.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -26,5 +31,8 @@ if(savedUsername===null){
 	loginForm.addEventListener("submit",onLoginSubmit);
 }else{
 	//Show the greetings
-	paintGreetings(savedUsername);
+	paintGreetings();
 }
+
+//savedUsername은 localStorage에 저장되어 있는 값에서 나옵니다.
+//username은 input에 들어온 값(loginInput.value)을 저장합니다.
