ce699d3d9f3f46c77855a9ecd7bcc2a280965909
차지태 <55056889+ChaJiTae1023@users.noreply.github.com>
2022-11-19 14:27:20 +0900

    #6.1 background random

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = ["0.gif","1.gif","2.gif","3.gif"];
+
+const chosenImage = images[Math.floor(Math.random()*images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src=`img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
