790ac267e90fa5820aaa7052fbe711b80618102f
차지태 <55056889+Ideal-JiTae@users.noreply.github.com>
2022-12-04 14:08:45 +0900

    #8.1 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,17 @@
+const API_KEY = "242f66d0fb6fb0c42d7a0f9d2926c0d1";
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("you live in "+ lat + lng)
+    const lon = position.coords.longitude;
+    const url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url)
+    .then((response)=>response.json())
+    .then((data)=>{
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError(){
